aab1e4da800679734c3ef1390a700cd68f8743e7
Preslav Mitroev <presalv1233@gmail.com>
2023-05-30 18:04:23 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig'); 
+const homeController = require('./controllers/homeController');
 
 const app = express();
-
+ 
 const PORT = 3000;
 
 expressConfig(app); 
 handlebarsConfig(app);
 
-//Routes
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
+app.use(homeController);
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
