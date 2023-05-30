b5aec5d4edbaecb903609c04cfada519d8751a91
Preslav Mitroev <presalv1233@gmail.com>
2023-05-30 17:12:02 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const handlerbars = require('express-handlebars');
 
 const app = express();
 
 const PORT = 3000;
 
+//Handlebars config
+app.engine('hbs', handlerbars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
 app.get('/', (req, res) => {
-    res.send('Hello from express');
+    res.render('index');
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
