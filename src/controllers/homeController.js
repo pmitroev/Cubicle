4f876c0a990bb4aea430f5822639610a6ea8babc
Preslav Mitroev <presalv1233@gmail.com>
2023-05-31 17:29:13 +0300

    add Search function

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,7 +3,9 @@ const cubeManager = require('../managers/cubeManager');
 
 
 router.get('/', (req, res) => {
-    const cubes = cubeManager.getAll();
+    const { search, from, to} = req.query;
+    
+    const cubes = cubeManager.getAll(search, from, to);
 
     res.render('index', { cubes });
 });
--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -16,7 +16,23 @@ const cubes = [
 ];
 const uniqid = require('uniqid');
 
-exports.getAll = () => cubes.slice();
+exports.getAll = (search, from, to) => {
+    let result = cubes.slice();
+
+    if (search) {
+        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if (from) {
+        result = result.filter(cube => cube.difficultyLevel >= Number(from));
+    }
+
+    if (to) {
+        result = result.filter(cube => cube.difficultyLevel <= Number(to));
+    }
+
+    return result;
+};
 
 exports.getOne = (cubeId) => cubes.find(x => x.id = cubeId);
 
