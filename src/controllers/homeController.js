a296f8d157cd97169e8ba6ef8c75758550cc237b
Preslav Mitroev <presalv1233@gmail.com>
2023-05-31 16:49:08 +0300

    adding details page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -10,6 +10,6 @@ router.get('/', (req, res) => {
 
 router.get('/about', (req, res) => {
     res.render('about');
-})
+});
 
 module.exports = router;
--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,24 @@
-const cubes = [];
+const cubes = [
+    {
+        id: '1n73shSholhz66elc',
+        name: 'Mirror Cube', 
+        description: 'A cool mirror cube',
+        imageUrl: 'https://m.media-amazon.com/images/I/71TrvUl50OL.jpg',
+        difficultyLevel: 4,
+    },
+    {
+        id: '2n73sh8holaz66elc', 
+        name: 'Rubic Classic', 
+        description: 'Evergreen',
+        imageUrl: 'https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067-675x380.jpg',
+        difficultyLevel: 3
+    }
+];
 const uniqid = require('uniqid');
 
 exports.getAll = () => cubes.slice();
+
+exports.getOne = (cubeId) => cubes.find(x => x.id = cubeId);
 
 exports.create = (cubeData) => {
     const newCube = {
