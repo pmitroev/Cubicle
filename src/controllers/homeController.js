e8c8d080769a511c855a8c1741318f207e9afa08
Preslav Mitroev <presalv1233@gmail.com>
2023-05-31 17:31:29 +0300

    remember search inputs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     
     const cubes = cubeManager.getAll(search, from, to);
 
-    res.render('index', { cubes });
+    res.render('index', { cubes, search, from, to });
 });
 
 router.get('/about', (req, res) => {
