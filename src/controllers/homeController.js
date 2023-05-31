722db3f75e4e9575485ca0dca527a5dcd3a11fa5
Preslav Mitroev <presalv1233@gmail.com>
2023-05-31 17:18:03 +0300

    Update homeController.js

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -9,11 +9,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about'); 
 });
 
 router.get('/404', (req, res) => {
-    res.render('404');
+    res.render('404');  
 });
 
 module.exports = router;
